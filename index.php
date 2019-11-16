0e3cb7d33c144a48ce1af98810b1bd92d1f93100
erich8367-hub <55596497+erich8367-hub@users.noreply.github.com>
2019-11-16 11:02:30 -0500

    1.02

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php 
-	session_start();
-	setcookie(session_name(), '', 100);
-	session_unset();
-	session_destroy();
-	$_SESSION = array();
+	session_start(); 
+
+	if (!isset($_SESSION['username'])) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: login.php');
+	}
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header("location: login.php");
+	}
 ?>
 
 <!DOCTYPE html>
